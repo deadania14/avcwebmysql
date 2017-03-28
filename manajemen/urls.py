d4637353e350bf473cbfc1d57f5c827c1856a2d7
deadania <deadania14@gmail.com>
2017-03-28 22:18:20 +0700

    success made article (new) and display

--- manajemen/urls.py
+++ manajemen/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^list_article/$', views.list_article, name='list_article'),
+    #/article_detail
+    url(r'^detail_article/(?P<article_id>[0-9]+)/$', views.detail_article, name='detail_article'),
+    #/new_article
+    url(r'^article/new/$', views.new_article, name='new_article'),
+    
 ]
--- manajemen/views.py
+++ manajemen/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Article
+from .forms import ArticleForm
 
 def index(request):
     context={}
     return render(request, 'manajemen/index.html', context)
+
 def list_article(request):
     context={}
-    article_query = Article.objects.all()
-    context['articles'] = article_query
+    articles_query = Article.objects.all()
+    context['articles'] = articles_query
     return render(request, 'manajemen/list_article.html', context)
+
+def detail_article(request, article_id):
+    context={}
+    articleid_query= Article.objects.get(id=article_id)
+    context['articleid'] = articleid_query
+    return render(request, 'manajemen/detail_article.html', context)
+
+def new_article(request):
+    if request.method=="POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            narticle = form.save(commit = False)
+            narticle.author = request.user
+            narticle.published_date= timezone.now()
+            narticle.save()
+            return redirect('detail_article', pk=narticle.pk)
+    else :
+        form = ArticleForm()
+    return render(request, 'manajemen/edit_article.html', {'form':form})
