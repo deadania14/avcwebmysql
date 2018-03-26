0b4a6357759114e5e42034ffd33423e76cd3aacc
deadania <deadania14@gmail.com>
2018-03-26 22:29:45 +0700

    database refresh

--- avcweb/settings.py
+++ avcweb/settings.py
@@ -92,6 +92,8 @@ WSGI_APPLICATION = 'avcweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite',
         'NAME': os.environ.get('DATABASE_NAME',''),
         'USER': os.environ.get('DATABASE_USER',''),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD',''),
