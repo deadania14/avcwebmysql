805c675e76f501535053d62c399eb74f25e55fff
deadania <deadania14@gmail.com>
2018-03-28 14:01:33 +0700

    os.env for host and port

--- avcweb/settings.py
+++ avcweb/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME',''),
         'USER': os.environ.get('DATABASE_USER',''),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD',''),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': os.environ.get('DATABASE_HOST',''),
+        'PORT': os.environ.get('DATABASE_PORT',''),
     }
 }
 
