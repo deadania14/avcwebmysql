f8fa0f34f00d004fe003c10ac734a092134ed84b
deadania <deadania14@gmail.com>
2018-08-16 14:20:22 +0700

    template mail fix1

--- avcweb/settings.py
+++ avcweb/settings.py
@@ -19,6 +19,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD','')
 EMAIL_PORT = 465
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 import django_extensions
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
